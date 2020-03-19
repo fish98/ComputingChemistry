425bcc84963cb5cf7492bac4af41122aea936106
fish <jiongchiyu@gmail.com>
2020-03-19 10:52:26 +0800

    refresh homework 3-1

---
+++ homework/1/T3_1_3170106187.py
@@ -0,0 +1,38 @@
+# coding:utf-8
+
+# 
+# Verify answers calculated by computer programs.
+#
+
+import math
+
+def main():
+
+#
+# Calculate quadratic equation solutions 
+# x^2 + (-10^t - 1)x + 10^t = 0
+#
+
+	t = int(input("Enter t\n"))
+	a = 1
+	b = pow(-10, t) - 1
+	c = pow(10, t)
+
+	x1 = (-1 * b + math.sqrt(b * b - 4 * a * c)) / 2 / a
+	x2 = (-1 * b - math.sqrt(b * b - 4 * a * c)) / 2 / a
+
+	print("Solution x1 = %d" % x1)
+	print("Solution x2 = %d" % x2)
+
+# 
+# Note 
+# Surprisingly found x2 equals 0 only when t is above 15 but not 11
+#
+
+if __name__ == '__main__':
+	main()
+
+#
+# Arthor: Jiongchi Yu
+# Date: 2020.3.18
+# 
